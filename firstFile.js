f513ae618d57ce0377418983936dd5b8b17aff03
Shuvo chakraborty <43523095+shuvo940@users.noreply.github.com>
2020-07-12 07:17:17 -0800

    constrcutor

--- firstFile.js
+++ firstFile.js
@@ -1,2 +1,31 @@
-const name = 'sumon';
-console.log(name);
+
+
+
+// const age1 = [10, 20, 30, 40];
+// const age2 = [100, 200, 300];
+
+// const fullages = [...age1, ...age2];
+// console.log(fullages)
+
+
+// const rahim = 500;
+// const karim = 700;
+
+// const result = Math.max(rahim, karim)
+// console.log(result)
+
+// const takapoisa = [100, 200, 300, 500]
+// const result = Math.max(...takapoisa)
+// console.log(result)
+
+class myClass {
+    constructor(sId, names) {
+        this.id = sId;
+        this.name = names;
+        this.commonscl = 'basurhat school'
+    }
+}
+
+const member1 = new myClass(22, 'nahid');
+console.log(member1.name)
+console.log(member1.id)
