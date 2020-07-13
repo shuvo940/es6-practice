06e06c9b9404e2e5b9ceb16bfc28c8d2939fa182
Shuvo chakraborty <43523095+shuvo940@users.noreply.github.com>
2020-07-13 11:06:41 -0800

    new code

--- firstFile.js
+++ firstFile.js
@@ -18,14 +18,93 @@
 // const result = Math.max(...takapoisa)
 // console.log(result)
 
-class myClass {
-    constructor(sId, names) {
-        this.id = sId;
-        this.name = names;
-        this.commonscl = 'basurhat school'
-    }
-}
+// class myClass { 
+//     constructor(sId, names) {
+//         this.id = sId;
+//         this.name = names;
+//         this.commonscl = 'basurhat school'
+//     }
+// }
 
-const member1 = new myClass(22, 'nahid');
-console.log(member1.name)
-console.log(member1.id)
+// const member1 = new myClass(22, 'nahid');
+// console.log(member1.name)
+// console.log(member1.id)
+
+
+// class ParentClass{
+//     constructor() {
+//         this.fatherName = 'dhanaranjan'
+//     }
+// }
+
+// class Childclass extends ParentClass{
+
+//     constructor(name) {
+//         super()
+//         this.name = name;
+
+//     }
+//     childParent() {
+//         return this.name + ' ' + this.fatherName;
+//     }
+// }
+
+// const childName = new Childclass('shuvo cb')
+// console.log(childName.childParent()) 
+
+
+// class parentClass{
+//     constructor() {
+//         this.fatherName = 'DhanaRanjan';
+//     }
+// }
+
+// class childClass extends parentClass{
+//     constructor(name) {
+//         super()
+//         this.name = name;
+//     }
+//     getFatherName() {
+//         return this.fatherName + ' ' + this.name;
+//     }
+// }
+
+// const childName = new childClass('Shuvo');
+// console.log(childName.getFatherName())
+
+
+// const arr = [
+//     { id: 22, name: "shuvo" },
+//     { id: 30, name: "rahim" },
+//     { id: 100, name: "abul" },
+//     {id: 200, name : "ratan"}
+// ]
+
+
+// const newArray = [];
+// for (let i = 0; i < students.length; i++){
+//     const temp = students[i]
+//     const nameObj = temp.name;
+//     const total = newArray.push(nameObj)
+//     console.log(total)
+
+// }
+// const names = students.map(n => n.name);
+// console.log(names)
+// const biger = students.filter(d => d.id > 30);
+// console.log(biger)
+
+
+
+// function myFucn(num1, num2) {
+
+//     return num1 + num2 + arguments[2]
+    
+    
+// }
+
+// const result = myFucn(10, 10,30);
+// console.log(result)
+
+
+
